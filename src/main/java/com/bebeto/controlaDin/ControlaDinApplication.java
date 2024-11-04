d1e67bc7859528d181bfa57bc30147eda2f5a1f6
Bebeto03 <rnxroberto@gmail.com>
2024-11-04 10:54:00 -0300

    inicialização projeto

---
+++ src/main/java/com/bebeto/controlaDin/ControlaDinApplication.java
@@ -0,0 +1,13 @@
+package com.bebeto.controlaDin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlaDinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlaDinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bebeto/controlaDin/ControlaDinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebeto.controlaDin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlaDinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
