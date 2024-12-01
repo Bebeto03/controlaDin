22cf30a56a9999d67e537178f6447e9b21ddcce9
Bebeto03 <rnxroberto@gmail.com>
2024-12-01 03:55:29 -0300

    incio do processo de criação de despesas.

--- src/main/java/com/bebeto/controlaDin/controller/ExpensesController.java
+++ src/main/java/com/bebeto/controlaDin/controller/ExpensesController.java
@@ -1,13 +1,31 @@
 package com.bebeto.controlaDin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bebeto.controlaDin.dto.DespesaDto;
+import com.bebeto.controlaDin.repository.DespesaRepository;
+import com.bebeto.controlaDin.repository.UsuarioRepository;
 
 @Controller
 public class ExpensesController {
 
+    @Autowired
+    private DespesaRepository despesaRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @GetMapping("/controlaDin/expenses")
     public String showExpensesPage(){
         return "expenses";
+    }
+
+    @GetMapping("/controlaDin/expenses/new")
+    public String showNewExpensePage(Model model){
+        model.addAttribute("despesaDto", new DespesaDto());
+        return "newExpense";
     }
 }
