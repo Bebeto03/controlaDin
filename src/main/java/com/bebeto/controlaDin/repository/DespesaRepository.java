8dfa5acf8a65ece4936d1fdcce923b408ff32374
Bebeto03 <rnxroberto@gmail.com>
2024-11-30 23:47:07 -0300

    busca de despesas e receitas associadas ao usuário da sessão

--- src/main/java/com/bebeto/controlaDin/repository/DespesaRepository.java
+++ src/main/java/com/bebeto/controlaDin/repository/DespesaRepository.java
@@ -1,9 +1,14 @@
 package com.bebeto.controlaDin.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bebeto.controlaDin.model.Despesa;
+import com.bebeto.controlaDin.model.Usuario;
 
 public interface DespesaRepository extends JpaRepository<Despesa, Long> {
 
+    public List<Despesa> findByUsuario(Usuario usuario);
+    
 }
--- src/main/java/com/bebeto/controlaDin/repository/ReceitaRepository.java
+++ src/main/java/com/bebeto/controlaDin/repository/ReceitaRepository.java
@@ -1,9 +1,14 @@
 package com.bebeto.controlaDin.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bebeto.controlaDin.model.Receita;
+import com.bebeto.controlaDin.model.Usuario;
 
 public interface ReceitaRepository extends JpaRepository<Receita, Long> {
+
+    public List<Receita> findByUsuario(Usuario usuario);
     
 }
